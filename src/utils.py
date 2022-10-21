e248e9c12ed0cd36e9f443688e913cffc541ebcf
Ivan Shanin <ivan.shanin@gmail.com>
2022-10-21 17:07:11 +0200

    weimar dataset demo added

--- src/utils.py
+++ src/utils.py
@@ -10,9 +10,9 @@ def load_config(filename):
         config = yaml.safe_load(f.read())
     return config
 
-def pianoroll(melody):
-    plt.figure(figsize=(25,5))
-    plt.scatter(melody['onset'], melody['pitch'], marker='.')
+#def pianoroll(melody):
+#    plt.figure(figsize=(25,5))
+#    plt.scatter(melody['onset'], melody['pitch'], marker='.')
 
 def comparative_pianoroll(melody, predictions, output_file = None, title = None, scale_factor = 0.7, height = 10):
     width = min(predictions.onset.max() * scale_factor, 400)
@@ -68,6 +68,60 @@ def comparative_pianoroll(melody, predictions, output_file = None, title = None,
         plt.savefig(output_file, bbox_inches='tight')
         plt.close()
 
+
+
+def pianoroll(melody, output_file = None, title = None, scale_factor = 0.7, height = 10):
+    width = min(melody.onset.max() * scale_factor, 400)
+    fig, ax = plt.subplots(figsize=(width, height))
+    
+    lowest_note = int(melody['pitch'].min())
+    highest_note = int(melody['pitch'].max())
+    first_onset = melody['onset'].min() - 2
+    last_onset = melody['onset'].max() + 2
+
+    for index in range(lowest_note - 1, highest_note + 1):
+        if index % 12 == 11:
+            color_ = 'gray'
+        else:
+            color_ = 'gainsboro'
+        ax.plot(
+            [first_onset, last_onset], 
+            [index + 0.5, index + 0.5], 
+            color = color_,
+            linewidth = 0.4,
+            label='_nolegend_'
+        )
+
+    for index in range(lowest_note, highest_note +1):
+        if (index % 12) in [1, 3, 6, 8, 10]:
+            ax.fill_between(
+                [first_onset, last_onset], 
+                [index - 0.5, index - 0.5], 
+                [index + 0.5, index + 0.5], 
+                color = 'whitesmoke',
+                label='_nolegend_'
+            )
+
+    ax.scatter(melody['onset'], melody['pitch'], marker='.', color = 'black')
+    
+    for _, row in melody.iterrows():
+        time = [row.onset, row.onset + row.duration]
+        pitch = [row.pitch, row.pitch]
+        ax.plot(time, pitch, color = 'black')
+
+    ax.set_yticks([24, 36, 48, 60, 72, 84])
+    ax.set_yticklabels(['C2', 'C3', 'C4', 'C5', 'C6', 'C7'])
+
+    #plt.legend(['SF NMF RCNN', 'ground truth'])
+    plt.xlabel('seconds')
+    plt.ylabel('pitch')
+    plt.xlim(first_onset, last_onset)
+    plt.ylim(lowest_note - 0.5, highest_note + 0.5)
+    if title:
+        plt.title(title)
+    if output_file:
+        plt.savefig(output_file, bbox_inches='tight')
+        plt.close()
 
 def play_audio(sample):
     print(sample['query'])
