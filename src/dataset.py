931b89295496a58e4836992e533372dedf9dbd2b
Ivan Shanin <ivan.shanin@gmail.com>
2022-11-17 19:47:17 +0100

    adapted dataloader for solo cuts

--- src/dataset.py
+++ src/dataset.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 from torch.utils.data import Dataset
 import torchaudio
 from torchaudio.transforms import Resample
@@ -8,6 +9,10 @@ import sqlite3
 import IPython.display as ipd
 import matplotlib.pyplot as plt
 from madmom.features.beats import RNNBeatProcessor, BeatTrackingProcessor
+from mir_eval.melody import resample_melody_series
+import warnings
+from sklearn.preprocessing import LabelBinarizer
+from utils import transcription2onehot
 
 WDB_SIZE = 456
 
@@ -27,6 +32,16 @@ MISTAKES = {
     "MilesDavis_Gingerbreadboy_Orig": "MilesDavis_GingerbreadBoy_Orig" 
 }
 
+SOLO_MISTAKES = {
+    "FatsNavarro_GoodBait_No1_Solo": "FatsNavarro_GoodBait_Solo",
+    "FatsNavarro_GoodBait_No2_Solo": "FatsNavarro_GoodBait_AlternateTake_Solo",
+    "BranfordMarsalis_Ummg_Solo": "BranfordMarsalis_U.M.M.G._Solo",
+    "SonnyRollins_I'llRememberApril-AlternateTake2_Solo": "SonnyRollins_I'llRememberApril_AlternateTake2_Solo",
+    "PaulDesmond_BlueRondoAlaTurk_Solo": "PaulDesmond_BlueRondoALaTurk_Solo"
+}
+
+SOLO_PATCH_FILES = ['LouisArmstrong_CornetChopSuey_Solo']
+
 SOLOSTART_CORRECTIONS = {
     43: 29.9,
     64: -15.345,
@@ -36,6 +51,47 @@ SOLOSTART_CORRECTIONS = {
     309: -212.5,
     382: 205.9
 }
+
+TRAIN_ARTISTS = [
+    'Warne Marsh', 'Benny Carter', 'Joe Lovano', 'Bix Beiderbecke',
+    'Von Freeman', 'Don Ellis', 'John Coltrane', 'Wynton Marsalis',
+    'Chet Baker', 'Clifford Brown', 'Lee Konitz', 'Charlie Shavers',
+    'Pat Martino', 'Harry Edison', 'Cannonball Adderley',
+    'Coleman Hawkins', 'George Coleman', 'Curtis Fuller',
+    'Charlie Parker', 'Lee Morgan', 'Roy Eldridge', 'Sonny Stitt',
+    'Chu Berry', 'Louis Armstrong', 'Art Pepper', 'Branford Marsalis',
+    'Pat Metheny', 'Steve Lacy', 'Nat Adderley', 'Buck Clayton',
+    'Milt Jackson', 'Red Garland', 'Zoot Sims', 'Lester Young',
+    'Dizzy Gillespie', 'John Abercrombie', 'Johnny Dodds',
+    'Ornette Coleman', 'J.J. Johnson', 'Sidney Bechet',
+    'Kenny Wheeler', 'Johnny Hodges', 'Rex Stewart',
+    'J.C. Higginbotham', 'Miles Davis', 'Chris Potter', 'Pepper Adams',
+    'Fats Navarro', 'Steve Turre', 'Gerry Mulligan', 'Michael Brecker',
+    'Herbie Hancock', 'Dickie Wells', 'Joe Henderson', 'Sonny Rollins',
+    'Woody Shaw', 'Eric Dolphy', 'Kai Winding', 'Phil Woods',
+    'Wayne Shorter', 'Kenny Garrett', 'David Murray', 'Lionel Hampton',
+    'Bob Berg'
+]
+
+TEST_OLD_ARTISTS = [
+    'Henry Allen', 'Dexter Gordon', 'Kenny Dorham', 'Benny Goodman',
+    'Paul Desmond', 'Don Byas', 'Ben Webster', 'Hank Mobley',
+    'Stan Getz', 'David Liebman', 'Steve Coleman', 'Kid Ory',
+    'Joshua Redman', 'Freddie Hubbard'
+]
+
+TEST_ARTISTS = [
+    'Ben Webster', 'Cannonball Adderley', 'Charlie Shavers', 'Chu Berry', 
+    'Chris Potter', 'Dexter Gordon', 'Fats Navarro', 'Joe Henderson', 
+    'John Coltrane', 'Kenny Wheeler', 'Paul Desmond', 'Sidney Bechet', 
+    'Sonny Rollins', 'Wynton Marsalis'
+]
+
+
+INSTRUMENTS = [
+    'cl', 'as', 'ts', 'cor', 'tp', 'tb', 'ss', 'bcl', 'bs', 'p',
+    'ts-c', 'g', 'vib'
+]
 
 class WeimarSolo(object):
     def __init__(self):
@@ -53,12 +109,39 @@ class WeimarSolo(object):
         self.performer = None
         self.title = None
         self.instrument = None
+        self.sfnmf = None
 
     def __str__(self):
-        return(self.filename)
+        return(f'{self.performer} ({self.instrument}) - {self.title} (from {self.filename})')
     
     def __repr__(self):
-        return(f'{self.performer} ({self.instrument}) - {self.title}')
+        return(f'{self.performer} ({self.instrument}) - {self.title} (from {self.filename})')
+
+    def fill_pauses(self):
+        onset = self.melody.onset
+        offset = self.melody.onset +  self.melody.duration
+        pitch = self.melody.pitch
+        rows = []
+        row = {
+            'pitch': 0,
+            'onset': 0,
+            'voicing': 0
+        }
+        rows.append(row)
+        for index in range(onset.shape[0]):
+            row = {
+                'pitch': pitch.iloc[index],
+                'onset': onset.iloc[index],
+                'voicing': 1
+            }
+            rows.append(row)
+            row = {
+                'pitch': 0,
+                'onset': offset.iloc[index],
+                'voicing': 0
+            }
+            rows.append(row)
+        return pd.DataFrame(rows)
 
     def resampled_audio(self):   
         transform = Resample(
@@ -66,6 +149,23 @@ class WeimarSolo(object):
             new_freq = self.resample_rate
         )
         return transform(self.audio)
+
+    def resampled_transcription(self, onehot = True):
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore')
+            ground_truth = self.fill_pauses()
+            times_new = np.arange(self.sfnmf.shape[1]) * 0.0116 
+            res_gt_pitch, res_gt_times = resample_melody_series(
+                ground_truth.onset,
+                ground_truth.pitch,
+                ground_truth.voicing,
+                times_new
+            )
+            if onehot:
+                return transcription2onehot(res_gt_pitch)
+            else:
+                return res_gt_pitch
+        
 
     def mono_audio(self):
         return self.audio.mean(axis = 0)
@@ -78,11 +178,11 @@ class WeimarSolo(object):
     def export_to_sv(self):
         pd.DataFrame(
             {
-                'onset': self.melody.onset + self.solostart,
+                'onset': self.melody.onset,
                 'duration': self.melody.duration,
                 'pitch': self.melody.pitch
             }
-        ).to_csv(self.filename + '.melody.csv', index = False)
+        ).to_csv(self.filename_solo + '.melody.csv', index = False)
 
     def play(self):
         print(self.__repr__())
@@ -144,33 +244,45 @@ class WeimarSolo(object):
         if output_file:
             plt.savefig(output_file, bbox_inches='tight')
             plt.close()
-
             
 
 class WeimarDB(Dataset):
-    def __init__(self, config):
+    def __init__(self, config, partition = 'full', instrument = 'any', autoload_audio = True, autoload_sfnmf = False):
         config_shared = config['shared']
         config_local = config['dataset']
+
+        self._patch_dir = os.path.join(
+            config_shared['raw_data'], 
+            config_local['patch_dir']
+        )
 
         self._audio_dir = os.path.join(
             config_shared['raw_data'], 
             config_local['audio_dir']
         )
 
+        self._sfnmf_dir = os.path.join(
+            config_shared['exp_folder'],
+            config_local['sfnmf_dir']
+        )
+
+        self.autoload_audio = autoload_audio
+        self.autoload_sfnmf = autoload_sfnmf
         self._resample_rate = config_local.get('resample_rate', None)
         self.load_beats = config_local.get('load_beats', False)
         self._init_column_names()
         self._init_database_cursor(config_local)
-        self._init_idx_dict(config_local)
+        self._init_melid_list(partition, instrument)
 
-
-    def _init_idx_dict(self, config):
-        if config['full']:
-            self._idx_dict = [elem for elem in range(1, WDB_SIZE + 1)]
-        else:
-            with open(config['subset'], 'r') as file:
-                line = file.readline()
-            self._idx_dict = [int(elem) for elem in line.split(',')]
+    def _init_melid_list(self, partition, instrument):
+        solo_info = self.get_solo_info()
+        if partition == 'train':
+            solo_info = solo_info[solo_info.performer.isin(TRAIN_ARTISTS)]
+        elif partition == 'test':
+            solo_info = solo_info[solo_info.performer.isin(TEST_ARTISTS)]
+        if instrument in INSTRUMENTS:
+            solo_info = solo_info[solo_info.instrument == instrument]
+        self._melid_list = solo_info.melid.values
 
 
     def _init_column_names(self):
@@ -217,6 +329,9 @@ class WeimarDB(Dataset):
         solo.solostart = max(transcription_info.solostart_sec.values[0], 0)
         solo.solostart += SOLOSTART_CORRECTIONS.get(solo.melid, 0)
         solo.solostop = solo.solostart + solo.melody.onset.max() + 2
+        solo.filename_solo = transcription_info.filename_solo.values[0]
+        for key in SOLO_MISTAKES:
+            solo.filename_solo = solo.filename_solo.replace(key, SOLO_MISTAKES[key])
 
     def _parse_solo_info(self, solo):
         colnames_str = ', '.join(self._solo_info_columns)
@@ -262,32 +377,61 @@ class WeimarDB(Dataset):
 
 
     def _load_audio(self, solo):
-        audio, solo.sample_rate = torchaudio.load(
+        if solo.filename_solo in SOLO_PATCH_FILES:
+            audio_dir = self._patch_dir
+        else:
+            audio_dir = self._audio_dir
+        solo.audio, solo.sample_rate = torchaudio.load(
             os.path.join(
-                self._audio_dir, 
-                f'{solo.filename}.wav'
+                audio_dir, 
+                f'{solo.filename_solo}.wav'
             )
         )
-        sample_start = floor(solo.solostart * solo.sample_rate)
-        if solo.solostop:
-            sample_stop = floor(solo.solostop * solo.sample_rate)
-        else:
-            sample_stop = -1
-        solo.audio = audio[:, sample_start : sample_stop]
+        #sample_start = floor(solo.solostart * solo.sample_rate)
+        #if solo.solostop:
+        #    sample_stop = floor(solo.solostop * solo.sample_rate)
+        #else:
+        #    sample_stop = -1
+        #solo.audio = audio
 
+    def _load_sfnmf(self, solo):
+        path = os.path.join(
+            self._sfnmf_dir,
+            f'melid_{str(solo.melid).zfill(3)}.npy'
+        )
+        solo.sfnmf = np.load(path)
+
+    def get_solo_info(self):
+        colnames_str = ', '.join(self._solo_info_columns)
+        query = f'SELECT {colnames_str} FROM solo_info'
+        return pd.DataFrame(
+            self._cursor.execute(query).fetchall(), 
+            columns = self._solo_info_columns
+        )
+
+    def get_transcription_info(self):
+        colnames_str = ', '.join(self._transcription_info_columns)
+        query = f'SELECT {colnames_str} FROM transcription_info'
+        return pd.DataFrame(
+            self._cursor.execute(query).fetchall(), 
+            columns = self._transcription_info_columns
+        )
 
     def __getitem__(self, idx):
         
         solo = WeimarSolo()
-        solo.melid = self._idx_dict[idx]
+        solo.melid = self._melid_list[idx]
         self._parse_melody(solo)
         self._parse_beats(solo)
         self._parse_transcription_info(solo)
         self._parse_track_info(solo)
         self._parse_solo_info(solo)
         solo.resample_rate = self._resample_rate
-        self._load_audio(solo)
+        if self.autoload_audio:
+            self._load_audio(solo)
+        if self.autoload_sfnmf:
+            self._load_sfnmf(solo)
         return solo
         
     def __len__(self):
-        return WDB_SIZE
+        return len(self._melid_list)
